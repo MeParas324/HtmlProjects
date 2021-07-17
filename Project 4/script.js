4284590fe0eb11b55c531640bf177f89104b9bc3
Paras <parasverma0527@gmail.com>
2021-07-17 06:44:32 +0530

    Initial commit

---
+++ Project 4/script.js
@@ -0,0 +1,11 @@
+// let taskbar = document.getElementsByClassName("taskbar")[0];
+// let taskbar = document.getElementsByClassName("startmenu")[0];
+
+// taskbar.addEventListener("click", ()=>{
+//     if(startmenu.style.button == "50px"){
+//         startmenu.style.button = "-490px";
+//     }
+//     else{
+//         startmenu.style.button = "50px";
+//     }
+// })
